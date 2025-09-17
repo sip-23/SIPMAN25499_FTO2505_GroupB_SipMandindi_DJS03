0dc2be0ce2f9543d72ea61322e04a7b93201ef68
sip-23 <ndiramandindi@gmail.com>
2025-09-17 02:03:29 +0200

    Learning how to import and export components

--- sippi-cup/src/App.jsx
+++ sippi-cup/src/App.jsx
@@ -1,24 +1,17 @@
 import { useState } from 'react'
-import sippiCupLogo from './assets/SippiCup_logo.png'
 import './App.css'
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={sippiCupLogo} className="logo-sippi-cup mx-auto mb-4" alt="Sippi-Cup logo" />
-        </a>
-      </div>
-      <h1>Count Up to 1000 to launch Sippi-Cup</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
   )
 }
 
